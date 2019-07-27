6b2b010afe563956c3cd1886abf10b46ef9f8a22
linfeng <jserlin@163.com>
2019-07-27 16:03:11 +0800

    详情 底部重写，订单 购物车完善。

--- src/pages/cart/cart.vue
+++ src/pages/cart/cart.vue
@@ -1,42 +1,73 @@
 <template>
   <div class="cart">
     <div class="top">
-      <div>30天无忧退货</div>
-      <div>48小时快速退款</div>
-      <div>满88元免邮费</div>
+      <div class="tip-item">30天无忧退货</div>
+      <div class="tip-item">>48小时快速退款</div>
+      <div class="tip-item">满88元免邮费</div>
     </div>
     <div class="cartlist">
       <div class="item" >
         <div class="con">
-          <div class="left">
-            <div class="icon active"></div>
-            <div class="img">
-              <img src="http://nos.netease.com/mailpub/hxm/yanxuan-wap/p/20150730/style/img/icon-normal/noCart-a8fe3f12e5.png" alt="">
-            </div>
-            <div class="info">
-              <p>商品</p>
-              <p>￥10</p>
-            </div>
+          <div class="icon"></div>
+          <div class="img-wrap">
+            <img class="img" src="http://nos.netease.com/mailpub/hxm/yanxuan-wap/p/20150730/style/img/icon-normal/noCart-a8fe3f12e5.png" alt="">
           </div>
-          <div class="right">
-            <div class="num">
-              x10
+          <div class="info">
+            <div class="name">黑科技·冰能持久冷感夏凉被</div>
+            <div class="desc">
+              <span class="span">浅卡其; 150*200cm</span>
+            </div>
+            <div class="price">￥10</div>
+          </div>
+        </div>
+        <div class="m-selNumRow">
+          <div class="m-selnum">
+            <div class="less">
+              <div class="span">-</div>
+            </div>
+            <div class="textWrap">
+              <input class="input" type="tel" value="1">
+            </div>
+            <div class="more">
+              <div class="span">+</div>
             </div>
           </div>
         </div>
-
-        <div class="delete">
-          <div>
-            删除
+      </div>
+      <div class="item" >
+        <div class="con">
+          <div class="icon active"></div>
+          <div class="img-wrap">
+            <img class="img" src="http://nos.netease.com/mailpub/hxm/yanxuan-wap/p/20150730/style/img/icon-normal/noCart-a8fe3f12e5.png" alt="">
+          </div>
+          <div class="info">
+            <div class="name">黑科技·冰能持久冷感夏凉被</div>
+            <div class="desc">
+              <span class="span">浅卡其; 150*200cm</span>
+            </div>
+            <div class="price">￥10</div>
           </div>
         </div>
-
+        <div class="m-selNumRow">
+          <div class="m-selnum">
+            <div class="less">
+              <div class="span">-</div>
+            </div>
+            <div class="textWrap">
+              <input class="input" type="tel" value="1">
+            </div>
+            <div class="more">
+              <div class="span">+</div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-    <div  class="nogoods">
+    <!-- 没商品时展示 -->
+    <!-- <div  class="nogoods" >
       <img src="http://nos.netease.com/mailpub/hxm/yanxuan-wap/p/20150730/style/img/icon-normal/noCart-a8fe3f12e5.png" alt="">
-    </div>
-    <div class="fixed">
+    </div> -->
+    <div class="foot-fixed">
       <div class="left allcheck">
         全选
       </div>
@@ -68,12 +99,19 @@ export default {
 
 <style scoped lang="less">
 .cart {
-  overflow-x: hidden;
+  position: relative;
+  padding-bottom: 104rpx;
+  font-family: PingFangSC-Light, helvetica, 'Heiti SC';
+  background: #fff;
+  background: #fafafa;
+  color: #333;
+  min-height: 100vh;
+  box-sizing: border-box;
   .top {
     display: flex;
     justify-content: space-between;
     padding: 30rpx 20rpx;
-    div {
+    .tip-item {
       background: url(http://nos.netease.com/mailpub/hxm/yanxuan-wap/p/20150730/style/img/icon-normal/servicePolicyRed-518d32d74b.png) 0 center no-repeat;
       background-size: 10rpx;
       padding-left: 15rpx;
@@ -89,48 +127,60 @@ export default {
     .item {
       padding: 20rpx 0;
       border-bottom: 1rpx solid #f4f4f4;
-      height: 166rpx;
       position: relative;
       .con {
         display: flex;
         align-items: center;
         justify-content: space-between;
         transition: all 300ms ease;
-        .left {
-          display: flex;
-          align-items: center;
-          width: 80%;
-          .icon {
-            height: 125rpx;
-            width: 34rpx;
-            background: url(http://nos.netease.com/mailpub/hxm/yanxuan-wap/p/20150730/style/img/icon-normal/checkbox-0e09baa37e.png) no-repeat center center;
-            background-size: 34rpx 34rpx;
-            margin: 0 20rpx;
-          }
-          .icon.active {
-            background: url(http://nos.netease.com/mailpub/hxm/yanxuan-wap/p/20150730/style/img/icon-normal/checkbox-checked-822e54472a.png) no-repeat center center;
-            background-size: 34rpx 34rpx;
-          }
+        .icon {
+          height: 125rpx;
+          width: 40rpx;
+          background: url(http://nos.netease.com/mailpub/hxm/yanxuan-wap/p/20150730/style/img/icon-normal/checkbox-0e09baa37e.png) no-repeat center center;
+          background-size: 40rpx 40rpx;
+          margin: 0 30rpx;
+        }
+        .icon.active {
+          background: url(http://nos.netease.com/mailpub/hxm/yanxuan-wap/p/20150730/style/img/icon-normal/checkbox-checked-822e54472a.png) no-repeat center center;
+          background-size: 40rpx 40rpx;
+        }
+        .img-wrap {
+          width: 170rpx;
+          height: 170rpx;
+          position: relative;
+          margin-right: 20rpx;
+          border-radius: 8rpx;
+          overflow: hidden;
+          background-color: #eee;
           .img {
-            height: 125rpx;
-            width: 125rpx;
-            display: block;
-            background: #f4f4f4;
-            img {
-              width: 100%;
-              height: 100%;
-            }
-          }
-          .info {
-            width: 50%;
-            padding: 20rpx;
-            p {
-              line-height: 40rpx;
-            }
+            width: 100%;
+            height: 100%;
           }
         }
-        .right {
-          padding-right: 50rpx;
+        .info {
+          flex: 1;
+          padding: 20rpx;
+          .name{
+            line-height: 36rpx;
+            font-size: 28rpx;
+          }
+          .desc{
+            color: #7F7F7F;
+            margin-top: 10rpx;
+            font-size: 24rpx;
+            line-height: 36rpx;
+            .span {
+              display: inline-block;
+              padding: 0 10rpx;
+              border: 1rpx solid #EFEFEF;
+              border-radius: .02667rem;
+              background: #FAFAFA;
+            }
+          }
+          .price{
+            padding-top: 30rpx;
+            font-size: 32rpx;
+          }
         }
       }
       .delete {
@@ -152,7 +202,7 @@ export default {
       }
     }
   }
-  .fixed {
+  .foot-fixed {
     position: fixed;
     bottom: 0;
     left: 0;
@@ -203,5 +253,44 @@ export default {
     }
   }
 }
-
+// 数量加减 样式
+.m-selNumRow{
+  position: absolute;
+  right: 30rpx;
+  bottom: 30rpx;
+  display: flex;
+  align-items: center;
+  .m-selnum{
+    display: inline-flex;
+    line-height: 66rpx;
+    text-align: center;
+    border: 1rpx solid #d9d9d9;
+    .less,.more{
+      position: relative;
+      width: 60rpx;
+      height: 50rpx;
+      text-align: center;
+      .span{
+        font-size: 30rpx;
+        line-height: 50rpx;
+      }
+    }
+    .textWrap{
+      width: 58rpx;
+      text-align: center;
+      border-left: 1rpx solid #d9d9d9;
+      border-right: 1rpx solid #d9d9d9;
+      .input{
+        height: 100%;
+        width: 100%;
+        box-sizing: border-box;
+        margin: 0;
+        text-align: center;
+        color: #333;
+        border: none;
+        background-color: transparent;
+      }
+    }
+  }
+}
 </style>
--- src/pages/order/submitOrder.vue
+++ src/pages/order/submitOrder.vue
@@ -1,20 +1,77 @@
 <template>
   <div class="container">
-      <van-card
-        num="2"
-        price="2.00"
-        desc="描述信息"
-        title="商品标题"
-        thumb="https://yanxuan.nosdn.127.net/0a053f1407b976b4b9523b837b15c247.png?imageView&quality=75&thumbnail=750x0"
-      />
-      <!-- 底部tab -->
-      <div class="footer-wrap">
-        <van-submit-bar
-          :price="price"
-          button-text="提交订单"
-          @submit="onSubmit"
-        />
+    <!-- 收货地址 -->
+    <div class="line-bg"></div>
+    <div class="address-wraper mb20">
+      <div class="address-content">
+        <div class="left">
+          <div class="name">范晓萱</div>
+          <div class="default">默认</div>
+        </div>
+        <div class="right">
+          <div class="right-item">176****2121</div>
+          <div class="right-item">北京市北京市东城区复兴道</div>
+        </div>
+        <div class="next">
+          <van-icon name="arrow" />
+        </div>
       </div>
+    </div>
+
+    <div class="product-wrap mb20">
+      <div class="head-title">已选商品</div>
+      <div class="content">
+          <div class="img-wrap">
+            <img class="img-item" src="https://yanxuan.nosdn.127.net/0a053f1407b976b4b9523b837b15c247.png?imageView&quality=75&thumbnail=750x0">
+          </div>
+          <div class="desc">
+              <div class="title">商品标题</div>
+              <div class="type">星空灰</div>
+              <div class="price">
+              <div class="price-now">￥11</div>
+                <div class="price-del">
+                  <div class="del"></div>
+                  ￥111
+                </div>
+              </div>
+          </div>
+          <div class="product-num">
+            x11
+          </div>
+      </div>
+      <div class="content">
+          <div class="img-wrap">
+            <img class="img-item" src="https://yanxuan.nosdn.127.net/0a053f1407b976b4b9523b837b15c247.png?imageView&quality=75&thumbnail=750x0">
+          </div>
+          <div class="desc">
+              <div class="title">商品标题</div>
+              <div class="type">星空灰</div>
+              <div class="price">
+              <div class="price-now">￥11</div>
+                <div class="price-del">
+                  <div class="del"></div>
+                  ￥111
+                </div>
+              </div>
+          </div>
+          <div class="product-num">
+            x11
+          </div>
+      </div>
+      <div class="foot">
+        <span class="span">应付金额</span>
+        <span class="span money">￥188</span>
+      </div>
+    </div>
+    <van-cell title="支付方式" value="微信支付" />
+    <!-- 底部tab -->
+    <div class="footer-wrap">
+      <van-submit-bar
+        :price="price"
+        button-text="立即购买"
+        @submit="onSubmit"
+      />
+    </div>
   </div>
 </template>
 <script>
@@ -38,6 +95,14 @@ export default {
     padding-bottom: 104rpx;
     font-family: PingFangSC-Light, helvetica, 'Heiti SC';
     background: #fff;
+    background: #fafafa;
+    color: #333;
+    min-height: 100vh;
+    box-sizing: border-box;
+}
+.img{
+  width: 100%;
+  height: 100%;
 }
 // 底部
 .footer-wrap {
@@ -49,5 +114,149 @@ export default {
   height: 104rpx;
   background: #fff;
   border-top: 1rpx solid #d9d9d9;
+}
+.mb20{
+  margin-bottom: 20rpx;
+}
+// 地址
+.line-bg {
+  display: block;
+  width: 100%;
+  height: 10rpx;
+  background: url(//yanxuan-static.nosdn.127.net/hxm/yanxuan-wap/p/20161201/style/img/icon-normal/address-bg-1579543565.png) repeat-x;
+  background-size: 60rpx 10rpx;
+}
+.address-wraper {
+  padding: 24rpx 0 8rpx;
+  background: #fff8d8;
+  .address-content {
+    position: relative;
+    padding-left: 30rpx;
+    display: flex;
+    align-items: center;
+    font-size: 28rpx;
+    .left {
+      margin-right: 20rpx;
+      align-self: flex-start;
+      line-height: 1;
+      color: #333;
+      .name {
+        margin-bottom: 16rpx;
+      }
+      .default {
+        padding: 0 5rpx;
+        font-size: 20rpx;
+        line-height: 28rpx;
+        text-align: center;
+        color: #b4282d;
+        border-radius: 6rpx;
+        border: 1rpx solid #b4282d;
+      }
+    }
+    .right {
+      flex: 1;
+      .right-item {
+        position: relative;
+        margin-right: 40rpx;
+        margin-bottom: 16rpx;
+        padding-left: 16rpx;
+        line-height: 1;
+        color: #7f7f7f;
+      }
+    }
+    .next {
+      padding: 0 20rpx;
+    }
+  }
+}
+.product-wrap{
+  background: #fff;
+  .head-title{
+    padding-top: 20rpx;
+    margin-left: 20rpx;
+    font-size: 28rpx;
+    line-height: 40rpx;
+    height: 40rpx;
+    color: #333;
+  }
+  .foot{
+    padding: 20rpx 0;
+    margin-right: 30rpx;
+    font-size: 28rpx;
+    line-height: 40rpx;
+    height: 40rpx;
+    text-align: right;
+    .span{
+      color: #333;
+    }
+    .money{
+      color: #b4282d;
+    }
+  }
+  // 商品列表
+  .content {
+    margin-left: 20rpx;
+    padding: 30rpx 10rpx 30rpx 0rpx;
+    display: flex;
+    align-items: center;
+    border-bottom: 1px solid #d9d9d9;
+    &:last-child {
+      border: none
+    }
+    .img-wrap {
+      margin-right: 40rpx;
+      width: 120rpx;
+      height: 120rpx;
+      background: #f4f4f4;
+      .img-item {
+        width: 120rpx;
+        height: 120rpx;
+      }
+    }
+    .desc {
+      flex: 1;
+      .title {
+        color: #333;
+        font-size: 28rpx;
+        margin: 4rpx 0;
+      }
+      .type {
+        font-size: 24rpx;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+        overflow: hidden;
+        color: #999;
+      }
+      .price {
+        display: inline-flex;
+        padding-top: 6rpx;
+        .price-now {
+          padding-right: 10rpx;
+          font-size: 28rpx;
+        }
+        .price-del {
+          position: relative;
+          color: #ccc;
+          font-size: 28rpx;
+          .del {
+            position: absolute;
+            left: 0;
+            bottom: 20rpx;
+            width: 100%;
+            height: 2rpx;
+            background: #ccc;
+          }
+        }
+      }
+    }
+    .red {
+      color: #f60
+    }
+    .product-num {
+      font-size: 28rpx;
+      align-self: flex-start;
+      margin-right: 30rpx;
+    }
+  }
 }
 </style>
